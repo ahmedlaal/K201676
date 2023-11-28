b2775882f34463c405d8798ac2aa2cd5a4ce388a
ahmedlaal <ahmedhamzakhaliq@gmail.com>
2023-11-28 06:19:25 -0800

    K201676 SCD Lab

---
+++ src/main/java/com/example/K201676/K201676Application.java
@@ -0,0 +1,13 @@
+package com.example.K201676;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K201676Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K201676Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/K201676/K201676ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.K201676;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K201676ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
